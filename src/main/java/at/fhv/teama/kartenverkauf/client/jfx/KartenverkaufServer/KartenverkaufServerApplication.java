e15d393aa6f11254216f56512b22bbda158c919c
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-16 15:34:42 +0200

    initial commit

---
+++ src/main/java/at/fhv/teama/kartenverkauf/client/jfx/KartenverkaufServer/KartenverkaufServerApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.teama.kartenverkauf.client.jfx.KartenverkaufServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartenverkaufServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartenverkaufServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhv/teama/kartenverkauf/client/jfx/KartenverkaufServer/KartenverkaufServerApplicationTests.java
@@ -0,0 +1,16 @@
+package at.fhv.teama.kartenverkauf.client.jfx.KartenverkaufServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KartenverkaufServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
