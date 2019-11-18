2e91fab37b35f52ba30a91c20e579f5697a390ab
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-11-18 08:54:58 +0100

    feature: hookup login to backend

--- src/main/java/at/fhv/teama/easyticket/client/jfx/views/login/LoginController.java
+++ src/main/java/at/fhv/teama/easyticket/client/jfx/views/login/LoginController.java
@@ -7,49 +7,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Slf4j
 @Component
 @Scope("prototype")
 @RequiredArgsConstructor
-@Slf4j
 public class LoginController implements Initializable {
   private final EasyTicketService easyTicketService;
-
-  @FXML
-  private TextField tfUsername;
-
-  @FXML
-  private PasswordField pfPassword;
-
-  @FXML
-  private Label username;
-
-  @FXML
-  private Label password;
-
-  @FXML
-  private Button btLogin;
+  @FXML private TextField tfUsername;
+  @FXML private PasswordField pfPassword;
+  @FXML private Button btLogin;
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {
-  }
+  public void initialize(URL location, ResourceBundle resources) {}
+
   @FXML
-  private void changeScene(ActionEvent event){
-    if (event.getSource() == btLogin){
+  private void changeScene(ActionEvent event) {
+    if (event.getSource() == btLogin) {
       MainView view = new MainView();
       Scene scene = new Scene(view.getView());
 
-      //stageinfo
+      // stageinfo
       Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
       window.setScene(scene);
       window.show();
@@ -57,12 +50,11 @@ public class LoginController implements Initializable {
   }
 
   @FXML
-  private void onLoginClick(ActionEvent event){
+  private void onLoginClick(ActionEvent event) {
     boolean success = login();
-    if (success){
+    if (success) {
       changeScene(event);
-    }else {
-
+    } else {
       Alert alert = new Alert(Alert.AlertType.ERROR);
       alert.setTitle("Achtung");
       alert.setHeaderText("Login fehlgeschlagen");
@@ -71,10 +63,18 @@ public class LoginController implements Initializable {
     }
   }
 
-  private boolean login(){
+  private boolean login() {
     String uname = tfUsername.getText();
     String pword = pfPassword.getText();
-    //return easyTicketService.login(uname, pword);
-    return true;
+    SecurityContext sc = SecurityContextHolder.getContext();
+    sc.setAuthentication(new UsernamePasswordAuthenticationToken(uname, pword));
+    log.info("Trying to log in as " + uname);
+    try {
+      easyTicketService.login(uname, pword);
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
   }
 }
